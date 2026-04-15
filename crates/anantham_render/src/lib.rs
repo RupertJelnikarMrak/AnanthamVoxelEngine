70e56314a9a5b07f07c0a39628b47e136f672d78
Savant <savant@nono.no>
2026-04-15 18:35:47 +0200

    First commit

---
+++ crates/anantham_render/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn hello_render() {
+    println!("Hello Render!");
+}
---
+++ crates/anantham_sandbox/src/main.rs
@@ -0,0 +1,7 @@
+use anantham_core::hello_core;
+use anantham_render::hello_render;
+
+fn main() {
+    hello_core();
+    hello_render();
+}
